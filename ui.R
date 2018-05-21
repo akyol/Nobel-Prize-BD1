b0eaab07e3e33c4d74e999939158f2c6feb6d7b3
Abraham Lee <abelee2016@gmail.com>
2018-05-21 15:12:01 -0700

    add basic structure

--- ui.R
+++ ui.R
@@ -1,6 +1,90 @@
 library(shiny)
+library(dplyr)
+library(plotly)
 
-# shinyUI with `fluidPage` layout
-shinyUI(fluidPage(
-  h1("UI Page"),
+shinyUI(navbarPage(
+  theme = "styles.css",
+  "Midwest Population",
+  tabPanel(
+    "Map",
+    titlePanel("Map of Recipients' Birthplaces"),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          "professor",
+          label = "Professor?",
+          choices = list(
+            "Yes" = "yes",
+            "No" = "no",
+            "Both" = "both"
+          ),
+          selected = "both"
+        ),
+        selectInput(
+          "gender_map",
+          label = "Gender",
+          choices = list(
+            "Male" = "male",
+            "Female" = "female",
+            "N/A" = "na"
+          ),
+          selected = "na"
+        ),
+        selectInput(
+          "country",
+          label = "Country",
+          choices = list(
+            "U.S."
+          )
+        )
+      ),
+      mainPanel(
+        plotlyOutput("map")
+      )
+    )
+  ),
+  tabPanel(
+    "Bar",
+    titlePanel("Number of Prizes Per Each Category"),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          "gender_bar",
+          label = "Gender",
+          choices = list(
+            "Male" = "male",
+            "Female" = "female",
+            "N/A" = "na"
+          )
+        ),
+        sliderInput(
+          "decade",
+          label = "Decade",
+          min = 1900,
+          max = 2010,
+          value = 1900,
+          step = 10
+        )
+      ),
+      mainPanel(
+        plotlyOutput("bar")
+      )
+    )
+  ),
+  tabPanel(
+    "Box",
+    titlePanel("Average Age of Recipients"),
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput(
+          "time_range", 
+          label = "Time Range",
+          min = 1901, max = 2018, value = c(1901, 2018)
+        )
+      ),
+      mainPanel(
+        plotlyOutput("box")
+      )
+    )
+  )
 ))
