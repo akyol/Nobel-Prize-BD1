de913ba954b016957a5bc41f2a842597beec4380
Samantha C <sschow@uw.edu>
2018-05-23 21:59:17 -0700

    started bar chart, can't connect it to server

--- server.R
+++ server.R
@@ -1,4 +1,16 @@
 library(shiny)
+library(ggplot2)
 
 shinyServer(function(input, output) {
+  
+  output$bar <- renderPlot({
+    source("/scripts/bar-chart.R")
+    gender <- all_data[[input$gender_bar]]
+    
+    bar_plot <- ggplot(data = all_data, aes(x = category)) +
+      geom_bar()
+    
+    bar_plot
+  })
+  
 })
