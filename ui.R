29902cddc2f9708fcf1dd3cebbe620f0930b594b
Yixin Xu <xyx0224@uw.edu>
2018-05-26 22:52:00 -0700

    made changes to boxplot

--- ui.R
+++ ui.R
@@ -21,15 +21,28 @@ shinyUI(navbarPage(
       ),
       br(),
       fluidRow(
-        tags$h5(id = "text", "Our project will be focused on analyzing data about Nobel Prize winners over the years. Some of the target audiences who might look at the visualizations we create could be current scholars who are curious about the qualities of a winner of a prize, or promoters of political justice worldwide.
-                      Questions that we would like to inform our audience based on this project are:
+        tags$h5(id = "text", "Our project will be focusing on analyzing data
+                      about Nobel Prize winners over the years. Some of the 
+                      target audiences who might look at the visualizations 
+                      we create could be current scholars who are curious about
+                      the qualities of a winner of a prize, or promoters of
+                      political justice worldwide.
+                      Questions that we would like to inform our audience based
+                      on this project are:
                       The countries that are represented by the winners
                       Which professors from US universities won Nobel prizes
                       The number of prizes awarded for each category
                       The percentage breakdown of how the awards are split
-                      People who passed away before being able to receive their prize
-                      We plan to create interactive map visualizations, tables, and charts in order to summarize the relevant data to answer these questions.
-                      The dataset we will be utilizing will be the NobelPrize API, because it details a lot of specific information about each recipient of these prizes. This API was created by Nobelprize.org, the Official Website of the Nobel Prize.")
+                      People who passed away before being able to receive their 
+                      prize
+                      We plan to create interactive map visualizations, tables,
+                      and charts in order to summarize the relevant data to 
+                      answer these questions.
+                      The dataset we will be utilizing will be the NobelPrize
+                      API, because it details a lot of specific information 
+                      about each recipient of these prizes. 
+                      This API was created by Nobelprize.org, the Official 
+                      Website of the Nobel Prize.")
       )
     )
   ),
@@ -127,7 +140,15 @@ shinyUI(navbarPage(
         )
       ),
       mainPanel(
-        plotlyOutput("box")
+        plotlyOutput("box"),
+        tags$div(
+          class = "summary", checked = NA,
+          tags$p("The boxplots illustrate the age distribution of Nobel Prize
+                 winners in total and by categories. The data being displayed
+                 change when the range of year selected changes so that
+                 you can see how the age distribution has changed, or has
+                 not changed, throughout the time. ")
+        )
       )
     )
   )
