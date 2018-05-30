274a2303d481885818300429f17b08e21c6ea174
Yixin Xu <xyx0224@uw.edu>
2018-05-30 08:54:49 -0700

    passed lintr close #10

--- scripts/boxplot.R
+++ scripts/boxplot.R
@@ -3,11 +3,13 @@ library(dplyr)
 library(plotly)
 
 # Build a box plot for average age by categories
-build_boxplot <- function (data, given_range) {
-  categories <- c("chemistry", "economics", "literature", "medicine", "peace",
-                  "physics")
-  data_in_range <- filter(data, born != "0000-00-00" & year >= given_range[1] & 
-                            year <= given_range[2])
+build_boxplot <- function(data, given_range) {
+  categories <- c(
+    "chemistry", "economics", "literature", "medicine", "peace",
+    "physics"
+  )
+  data_in_range <- filter(data, born != "0000-00-00" & year >= given_range[1] &
+    year <= given_range[2])
   total_born_year <- data_in_range$born
   total_born_year <- as.numeric(substr(total_born_year, 1, 4))
   avg_age <- as.numeric(data_in_range$year) - total_born_year
@@ -19,14 +21,14 @@ build_boxplot <- function (data, given_range) {
     age <- prize_year - born_year
     return(age)
   }
-  
+
   physics <- extract_age("physics")
   chemistry <- extract_age("chemistry")
   economics <- extract_age("economics")
   literature <- extract_age("literature")
   medicine <- extract_age("medicine")
   peace <- extract_age("peace")
-  
+
   if (length(economics) == 0) {
     boxplot <- plot_ly(type = "box") %>%
       add_boxplot(y = avg_age, name = "total") %>%
@@ -35,15 +37,16 @@ build_boxplot <- function (data, given_range) {
       add_boxplot(y = literature, name = "literature") %>%
       add_boxplot(y = medicine, name = "medicine") %>%
       add_boxplot(y = peace, name = "peace") %>%
-      layout(title = "Age Distribution by Prize Categories",
-             margin = list(b = 80),
-             xaxis = list(
-               title = "Category"
-             ),
-             yaxis = list(
-               autotick = TRUE,
-               title = "Age"
-             )
+      layout(
+        title = "Age Distribution by Prize Categories",
+        margin = list(b = 80),
+        xaxis = list(
+          title = "Category"
+        ),
+        yaxis = list(
+          autotick = TRUE,
+          title = "Age"
+        )
       )
   } else {
     boxplot <- plot_ly(type = "box") %>%
@@ -54,21 +57,17 @@ build_boxplot <- function (data, given_range) {
       add_boxplot(y = medicine, name = "medicine") %>%
       add_boxplot(y = peace, name = "peace") %>%
       add_boxplot(y = economics, name = "economics") %>%
-      layout(title = "Age Distribution by Prize Categories",
-             margin = list(b = 80),
-             xaxis = list(
-               title = "Category"
-             ),
-             yaxis = list(
-               autotick = TRUE,
-               title = "Age"
-             )
+      layout(
+        title = "Age Distribution by Prize Categories",
+        margin = list(b = 80),
+        xaxis = list(
+          title = "Category"
+        ),
+        yaxis = list(
+          autotick = TRUE,
+          title = "Age"
+        )
       )
   }
   return(boxplot)
 }
-
-# Test the function out
- #data <- 
-  #read.csv("/Users/YixinXu/Desktop/INFO201/Nobel-Prize-BD1/data/laureate.csv")
- #build_boxplot(data, c(1901, 1980))
