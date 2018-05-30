9120f1f7a1c448d83d09a8a40c7c30516c2595f5
Yixin Xu <xyx0224@uw.edu>
2018-05-30 13:52:31 -0700

    passed lintr

--- scripts/bar-chart.R
+++ scripts/bar-chart.R
@@ -7,29 +7,26 @@ prize_resource <- "/prize.csv"
 prizes <- read.csv(paste0(base_uri, prize_resource), stringsAsFactors = FALSE)
 
 laureate_resource <- "/laureate.csv"
-laureates <- read.csv(paste0(base_uri, laureate_resource), 
+laureates <- read.csv(paste0(base_uri, laureate_resource),
                       stringsAsFactors = FALSE)
 
 prizes <- select(prizes, id, category, year)
 laureates <- select(laureates, id, gender)
 
-all_data <- left_join(prizes,laureates, by = "id") 
+all_data <- left_join(prizes, laureates, by = "id")
 
 
 build_bar <- function(gender_input, decade_input){
-  
-  if(gender_input == "na") {
+  if (gender_input == "na") {
     data_in_range <- filter(all_data, year <= decade_input)
   } else {
-    data_in_range <- filter(all_data, gender == gender_input, 
+    data_in_range <- filter(all_data, gender == gender_input,
                             year <= decade_input)
   }
-  
-  prize_count <- group_by(data_in_range, category) %>% 
+  prize_count <- group_by(data_in_range, category) %>%
     summarize(
       num = n()
     )
-  
   plot_ly(
     x = prize_count$category,
     y = prize_count$num,
@@ -41,12 +38,8 @@ build_bar <- function(gender_input, decade_input){
                              "rgb(194, 172, 166)",
                              "rgb(233, 191, 223)",
                              "rgb(170, 206, 159)"))
-    ) %>% 
+    ) %>%
     layout (title = "Number of Recipients per Prize",
             xaxis = list(title = "Category"),
-            yaxis = list(title = "Number of Recipients"))  
-    
+            yaxis = list(title = "Number of Recipients"))
 }
-
-#build_bar("male", 1950)
-
